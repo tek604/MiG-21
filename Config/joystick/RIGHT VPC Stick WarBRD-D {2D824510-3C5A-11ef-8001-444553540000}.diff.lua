d28d95de2a34601fc39dc338c0ba2372cd021ebc
tek_604 <tek.604@gmail.com>
2024-08-14 11:58:03 +0200

    Added first version of config files

---
+++ Config/joystick/RIGHT VPC Stick WarBRD-D {2D824510-3C5A-11ef-8001-444553540000}.diff.lua
@@ -0,0 +1,98 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3185pnilu3185cd42vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Gun reload - 1",
+		},
+		["d3186pnilu3186cd42vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "Gun reload - 2",
+		},
+		["d3187pnilu3187cd42vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "Gun reload - 3",
+		},
+		["d3191pnilu3191cd42vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Fire Gun",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+		["d3192pnilunilcd42vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN30",
+				},
+			},
+			["name"] = "Weapon Release Cover Close",
+		},
+		["d3192pnilunilcd42vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Weapon Release Cover Open",
+		},
+		["d3193pnilu3193cd42vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Weapon Release",
+		},
+		["d74pnilu75cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN31",
+				},
+			},
+			["name"] = "Wheel Brakes",
+		},
+		["d824pnilunilcd42vdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+			["name"] = "Select next weapon",
+		},
+		["d825pnilunilcd42vdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "Select previous weapon",
+		},
+		["dnilp36unilcdnilvdnilvpnilvunil"] = {
+			["name"] = "View Center",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+		},
+	},
+}
+return diff
